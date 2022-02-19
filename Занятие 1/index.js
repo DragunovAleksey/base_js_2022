886e28955d9a22d7238178660f0454939265004c
Dragunov Aleksey <dragunof1@yandex.ru>
2022-02-19 11:33:51 +0300

    main

---
+++ Занятие 1/index.js
@@ -0,0 +1,12 @@
+console.log("hello javascript");
+const ourPlanetName = "Земля"
+let userCurrentTime = 25
+let articlesNumber = 2
+let paymentType = "наличка"
+let firstName = "Алексей"
+let middleName = "Валерьевич"
+let secondName = "Драгунов"
+
+
+
+
