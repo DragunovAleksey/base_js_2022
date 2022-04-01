92c7071fa28518f79ef4731e27d9205351331ba0
Dragunov Aleksey <dragunof1@yandex.ru>
2022-04-01 16:11:06 +0300

    01.04.22

--- lesson7/index.js
+++ lesson7/index.js
@@ -3,34 +3,42 @@
 	
     let filterArr = args.filter((elem) => (elem === parseInt(elem)) && (elem > 0) && (elem % 2 === 0));
     console.log(filterArr);
-
+//решение  с интернета
     /* let endArr = filterArr.map(e=>x+=e, x=0).reverse()[0];
     console.log(endArr); */
-
+//решение
     /*let sum = 0;
     for (let i = 0; i < filterArr.length; i++){
         sum += filterArr[i];
         }
     console.log(sum);
-
 }
- 
 myFirstFunc(1,2,22,"20",5 ,-44,37 ,55, 43n,98,76888);  */
 
-//Функция принимаеи массив чисел, убирает повторяющиеся значения и возвращает новый массив из оставшихся значений
+//Функция принимает массив чисел, убирает повторяющиеся значения и возвращает новый массив из оставшихся значений
 
-/* let testArr = [1,2,3,4,6,6,6,6,5,6,7,8]
-let mySecondFunc = function (...array) {
-    let endArr = array.filter((element, index) => {
-    return array.indexOf(element) === index;
-    })
-    console.log(endArr);
+
+/* let mySecondFunc = function (testArr) {
+    let endArr = [];
+    testArr.forEach((element) => {
+    if (!endArr.includes(element)) {
+        endArr.push(element);        
+    }    
+    });
+    return endArr
 }
+let testArray = [1,2,3,4,6,6,6,798, 245,6,5,6,7,8];
+console.table(mySecondFunc(testArray)); */
 
-mySecondFunc(testArr); */
-let chars = ['A', 'B', 'A', 'C', 'B'];
-let uniqueChars = chars.filter((element, index) => {
-    return chars.indexOf(element) === index;
-});
-console.log(uniqueChars);
-https://techrocks.ru/2021/09/12/how-to-remove-duplicates-in-an-array-in-js/
+// функция принимает два массива, проверяет на идентичность значений и индексов, возвращает true или false.
+
+let myThirdfunc = function (firstArr, secondArr) {
+    for (let i = 0; i < firstArr.length; i++) {
+            if ((firstArr.length !== secondArr.length) || (firstArr[i] !== secondArr[i])) {
+            return false;
+    } else return true;
+}
+}
+let firstArray = [1,2,3,4,5,6];
+let secondArray = [1,2,3,4,5];
+console.log(myThirdfunc(firstArray, secondArray));
